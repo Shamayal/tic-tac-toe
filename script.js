65867e110e6ba7afa29fb06c42e5f82b388c3a9e
Shamayal Syed <shamayalsyed9@gmail.com>
2025-11-23 21:13:24 -0500

    Prevent overwriting squares by adding boardState and gameActive checks in click handler

--- script.js
+++ script.js
@@ -35,17 +35,16 @@ const winPatterns = [
 squares.forEach((square, index) => {
   square.addEventListener("click", () => {
     console.log(`Square ${index + 1} clicked by ${currentPlayer}`);
-    if (currentPlayer === "X") {
-      square.innerText = "X";
-      boardState[index] = "X";
-      console.log(boardState);
-      changePlayer();
-    } else {
-      square.innerText = "O";
-      boardState[index] = "O";
-      console.log(boardState);
-      changePlayer();
-    }
+
+    // Prevent overwriting on a square
+    if (boardState[index] !== "" || !gameActive) return;
+
+    square.innerText = currentPlayer;
+    boardState[index] = currentPlayer;
+    console.log(boardState);
+    // TODO: check if player won
+    changePlayer();
+   
   });
 });
 
